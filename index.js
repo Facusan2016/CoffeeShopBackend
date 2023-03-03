13b09a37741ace0e5337d199c6d4443e579c03c9
Facundo <facusan2016@gmail.com>
2023-03-03 07:35:00 -0300

    CORS policy

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.static('public'));
 
-var  MyAllowSpecificOrigins = ["https://beanscene.netlify.app", "http://localhost:5173"];
-
-app.use(cors());
-app.options('*', cors());
+app.use(cors('*'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
